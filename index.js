e310a61acc4066d297a4f02ae6b29e3d59c9601c
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-11-11 20:26:54 +0600

    Update status part , delete operation and order part

--- index.js
+++ index.js
@@ -22,8 +22,26 @@ async function run() {
                 console.log("Database Connected MongoDB")
                 const database = client.db('SKY_LAMP');
                 const productsCollection = database.collection('product');
+                // Orders Collection:
+                const orderCollection = database.collection('orders');
 
-                // POST API:
+                // GET All Products API:
+                app.get('/products', async (req, res) => {
+                        const cursor = productsCollection.find({});
+                        const products = await cursor.toArray();
+                        res.send(products);
+                })
+
+                // GET Single Product:
+                app.get('/products/:id', async (req, res) => {
+                        const id = req.params.id;
+                        console.log("Getting Specific Service", id)
+                        const query = { _id: ObjectId(id) };
+                        const product = await productsCollection.findOne(query);
+                        res.json(product);
+                })
+
+                // POST Products API:
                 app.post('/products', async (req, res) => {
                         const product = req.body;
                         console.log('Hit The API', product);
@@ -31,8 +49,53 @@ async function run() {
                         const result = await productsCollection.insertOne(product);
                         console.log(result);
                         res.json(result);
+                });
+
+                // Orders Collection
+                // Add Orders API:
+                app.post('/orders', async (req, res) => {
+                        const order = req.body;
+                        const result = await orderCollection.insertOne(order);
+                        res.json(result);
                 })
 
+                // Update Status:
+                app.put('/orders/:id', async (req, res) => {
+                        const id = req.params.id;
+                        const updateStatus = req.body;
+                        const filter = { _id: ObjectId(id) }
+                        const options = { upsert: true };
+                        // create a document that sets the plot of the movie
+                        const updateDoc = {
+                                $set: {
+                                        status: 'Shipped'
+                                },
+                        };
+                        const result = await orderCollection.updateOne(filter, updateDoc, options);
+                        console.log("Updating User", req)
+                        res.json(result)
+                })
+
+                // GET ALL Orders:
+                app.get('/orders', async (req, res) => {
+                        const cursor = orderCollection.find({})
+                        const orders = await cursor.toArray();
+                        res.send(orders);
+                })
+                // Delete Operation:
+                app.get('/orders/:id', async (req, res) => {
+                        const id = req.params.id;
+                        const query = { _id: ObjectId(id) };
+                        const result = await orderCollection.findOne(query)
+                        res.json(result);
+                })
+
+                app.delete('/orders/:id', async (req, res) => {
+                        const id = req.params.id;
+                        const query = { _id: ObjectId(id) };
+                        const result = await orderCollection.deleteOne(query)
+                        res.json(result);
+                })
         }
         finally {
                 // client.close();
